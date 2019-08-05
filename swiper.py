d77e0631472960067fb7e825f032bc44383acf77
RyChrome <dilsy3@gmail.com>
2019-08-05 00:30:39 -0400

    * made it swipe right 50 times for default instead of 200 * added time.sleep() so tinder doesn't block you from swiping for a short duration

--- swiper.py
+++ swiper.py
@@ -1,12 +1,14 @@
 import pyautogui
+import time
 
 
 # clean code hehe xd
 def click_this_many_times(number):
     for i in range(number):
+        time.sleep(0.2)
         pyautogui.click(point_of_like_button)
 
 
 coordinates_of_like_button = pyautogui.locateOnScreen('like.png')
 point_of_like_button = pyautogui.center(coordinates_of_like_button)
-click_this_many_times(200)
+click_this_many_times(50)
